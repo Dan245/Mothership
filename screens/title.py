715f5065c5aa19ec6842ca60efa739fae93dd103
Dan245 <dvmmamo@gmail.com>
2022-01-10 14:30:30 -0500

    Create buttons for title screen

--- screens/title.py
+++ screens/title.py
@@ -9,19 +9,14 @@ import pygame
 # for i in range(1,5):
 #     button_y.append(button_y[i-1]+s_h/12)
 # button_size = (s_w/3, s_h/15)
-title_text = Text("Mothership", GINGA_INTER, WHITE, [2, 4], [1.5, 4])
-# host_button = Button("Host Game", GINGA_INTER, WHITE, 5, WHITE, s_w/2, button_y[0], button_size)
-# find_button = Button("Find Game", GINGA_INTER, WHITE, 5, WHITE, s_w/2, button_y[1], button_size)
-# options_button = Button("Options", GINGA_INTER, WHITE, 5, WHITE, s_w/2, button_y[2], button_size)
-# tutorial_button = Button("How to Play", GINGA_INTER, WHITE, 5, WHITE, s_w/2, button_y[3], button_size)
-# quit_button = Button("Quit", GINGA_INTER, WHITE, 5, WHITE, s_w/2, button_y[4], button_size)
-#
-# buttons = pygame.sprite.Group()
-# buttons.add(host_button)
-# buttons.add(find_button)
-# buttons.add(options_button)
-# buttons.add(tutorial_button)
-# buttons.add(quit_button)
+title_text = Text("Mothership", MEDABOTS, WHITE, [2, 4], [1.5, 4])
+host_button = Button("Host Game", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
+find_button = Button("Find Game", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
+options_button = Button("Options", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
+tutorial_button = Button("How to Play", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
+quit_button = Button("Quit", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
+
+buttons = [host_button, find_button, options_button, tutorial_button, quit_button]
 
 def run(screen):
     s_w = screen.get_width()
@@ -30,6 +25,5 @@ def run(screen):
 
     title_text.update()
 
-    # buttons.draw(screen)
-    # for button in buttons:
-    #     button.text.display(button.rect.center[0], button.rect.center[1])
+    for button in buttons:
+        button.update()
