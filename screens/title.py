c4a4e69e73121b84476609f549b118b12bae7f6d
Dan245 <dvmmamo@gmail.com>
2022-01-10 14:35:27 -0500

    Format button positions

--- screens/title.py
+++ screens/title.py
@@ -10,11 +10,11 @@ import pygame
 #     button_y.append(button_y[i-1]+s_h/12)
 # button_size = (s_w/3, s_h/15)
 title_text = Text("Mothership", MEDABOTS, WHITE, [2, 4], [1.5, 4])
-host_button = Button("Host Game", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
-find_button = Button("Find Game", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
-options_button = Button("Options", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
-tutorial_button = Button("How to Play", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
-quit_button = Button("Quit", GINGA_INTER, WHITE, WHITE, [2, 2], [3, 18])
+host_button = Button("Host Game", GINGA_INTER, WHITE, WHITE, [2, 12/6], [3, 18])
+find_button = Button("Find Game", GINGA_INTER, WHITE, WHITE, [2, 12/7], [3, 18])
+options_button = Button("Options", GINGA_INTER, WHITE, WHITE, [2, 12/8], [3, 18])
+tutorial_button = Button("How to Play", GINGA_INTER, WHITE, WHITE, [2, 12/9], [3, 18])
+quit_button = Button("Quit", GINGA_INTER, WHITE, WHITE, [2, 12/10], [3, 18])
 
 buttons = [host_button, find_button, options_button, tutorial_button, quit_button]
 
